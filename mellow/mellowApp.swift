56ccf23ebe5de6641eb0f5a667292ab8adc978b2
Tripp Gordon <jgordo17@uvm.edu>
2021-01-24 18:34:43 -0700

    re init

---
+++ mellow/mellowApp.swift
@@ -0,0 +1,17 @@
+//
+//  mellowApp.swift
+//  mellow
+//
+//  Created by Tripp Gordon on 1/24/21.
+//
+
+import SwiftUI
+
+@main
+struct mellowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
